506fa2becedb696fe0ab22ab1be4a5b418681a10
sweetcolor <sweet3color@gmail.com>
2015-08-10 11:11:51 +0300

    add tickets controller convert erb to slim

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tickets/new'
+
+  get 'tickets/create'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
